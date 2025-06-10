a2a40f745359194f3b4d5ed01d1b76bda0a4bf9f
joaocmezari <joaocmezari@gmail.com>
2025-06-10 16:18:18 -0300

    retirando os arquivos de controllers

--- sistema-cursos-programacao/backend_sistema_cursos/controllers/course_controller.py
+++ sistema-cursos-programacao/backend_sistema_cursos/controllers/course_controller.py
@@ -1,9 +1,1 @@
-from flask import Blueprint, request, jsonify
-from services.course_service import get_all_courses
 
-course_controller = Blueprint('course_controller', __name__)
-
-@course_controller.route('/courses', methods=['GET'])
-def list_courses():
-    courses = get_all_courses()
-    return jsonify(courses)
--- sistema-cursos-programacao/backend_sistema_cursos/controllers/user_controller.py
+++ sistema-cursos-programacao/backend_sistema_cursos/controllers/user_controller.py
@@ -1,15 +1,1 @@
-from flask import Blueprint, request, jsonify
-from services.user_service import get_all_users, create_user
 
-user_controller = Blueprint('user_controller', __name__)
-
-@user_controller.route('/users', methods=['GET'])
-def list_users():
-    users = get_all_users()
-    return jsonify(users)
-
-@user_controller.route('/users', methods=['POST'])
-def add_user():
-    data = request.json
-    result = create_user(data)
-    return jsonify(result), 201
